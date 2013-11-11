8ed343b4813367c1aa1ca5b15c26ea27d3bbc226
akizuki230 <guilloalva230@gmail.com>
2013-11-11 03:10:19 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Locxue::Application.config.secret_key_base = '5ac63a3def131a3a406d702a09b0beb5cadd624cca2048d8d2edb1614359f48c884a1eae4ebb038ca498f85b63e34d1be965abb89912dc8e4f882a6ec88c7397'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Locxue::Application.config.session_store :cookie_store, key: '_locxue_session'
